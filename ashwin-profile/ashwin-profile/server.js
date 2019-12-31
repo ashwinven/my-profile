0116246af2efe4e488a5021af765175bfb080664
Ashwin Venkata Krishnan <venkatakrishnan.a@husky.neu.edu>
2019-12-31 00:04:22 -0500

    refs #heroku commit

--- ashwin-profile/ashwin-profile/server.js
+++ ashwin-profile/ashwin-profile/server.js
@@ -1,5 +1,16 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/personal-portfoli-angular'));
+
+app.get('/*', function(_req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/personal-portfoli-angular/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
